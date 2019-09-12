c238a5735d2aa4107f1c4bc56cefe7696bcd4b88
Valentin447 <valentin@myttk.ru>
2019-09-12 13:56:05 +0700

    Created an initial project template

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import $ from 'jquery';
+import './scss/style.scss';
+import './scss/utils/fonts.scss';
+//import './pug/index.pug';
+
+//$('.title').html('Some text!355')
---
+++ webpack.base.conf.js
@@ -0,0 +1,90 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const PATHS = {
+  src: path.join(__dirname, './src'),
+  dist: path.join(__dirname, './dist'),
+  assets: 'assets',
+}
+
+module.exports = {
+
+  externals: {
+    paths: PATHS
+
+  },
+
+    entry: './src/index.js',
+    output: {
+        path: PATHS.dist,
+        filename: `${PATHS.assets}/js/[name].js`,
+    },
+    module: {
+        rules: [
+          {
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: '/node_modules/'
+          },
+          {
+            test: /\.(png|jpg|gif|svg)$/,
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]'
+            }
+          },
+          {
+            test: /\.pug$/,
+            loader: 'pug-loader'
+          },
+          {
+            test: /\.scss$/,
+            use: [
+              'style-loader',
+                MiniCssExtractPlugin.loader,
+                {
+                  loader: 'css-loader',
+                  options: {sourceMap: true},
+                },
+                {
+                  loader: 'sass-loader',
+                  options: {sourceMap: true},
+                }
+            ],
+          }, 
+         
+          {
+            test: /\.css$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                "css-loader"
+            ],
+          },  {
+            test: /\.(eot|svg|ttf|woff|woff2)$/,
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    name: './assets/fonts/[name].[ext]',
+                }
+            }]
+          }, 
+        ],
+      },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: `${PATHS.src}/pug/pages/index/index.pug`,
+            filename: './index.html',
+            inject: 'body',
+            hash: false
+        }),
+        new MiniCssExtractPlugin({
+            filename: `${PATHS.assets}/css/[name].css`,
+        }),
+        new CopyWebpackPlugin([
+          {from:`${PATHS.src}/static/fonts/`, to:`${PATHS.assets}/fonts/`}
+        ])
+    ],
+
+};
