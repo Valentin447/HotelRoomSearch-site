8447122eb41c0fc245815004f340899319dac881
Valentin447 <valentin@myttk.ru>
2020-01-11 22:18:56 +0700

    Still remake dropdowns

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './scss/style.scss';
 import './scss/utils/fonts.scss';
 //import './pages/UiKit/ColorsAndType/ColorsAndType.scss';
+import './pages/UiKit/FormElements/FormElements';
---
+++ src/pages/UiKit/FormElements/FormElements.js
@@ -0,0 +1,5 @@
+let dropdownOpen = document.querySelectorAll('.FormElements .dropdown_rooms .dropdown__list');
+dropdownOpen[1].className += " dropdown__list_open";
+
+let dropdownToogleNoRadius = document.querySelectorAll('.FormElements .dropdown_rooms .dropdown__toggle');
+dropdownToogleNoRadius[1].className += " dropdown__toogle_noRadius";
