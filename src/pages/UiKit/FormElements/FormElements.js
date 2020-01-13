1157a0a011198349b25b321b97b263bd27fcf54f
Valentin447 <valentin@myttk.ru>
2020-01-13 22:09:41 +0700

    Completed dropdown bedroom on Form elemets

--- src/pages/UiKit/FormElements/FormElements.js
+++ src/pages/UiKit/FormElements/FormElements.js
@@ -3,3 +3,4 @@ dropdownOpen[1].className += " dropdown__list_open";
 
 let dropdownToogleNoRadius = document.querySelectorAll('.FormElements .dropdown_rooms .dropdown__toggle');
 dropdownToogleNoRadius[1].className += " dropdown__toogle_noRadius";
+
