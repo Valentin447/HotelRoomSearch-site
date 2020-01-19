3c7b578207193319be3ce88eee483f62b76cae26
Valentin447 <valentin@myttk.ru>
2020-01-19 17:24:17 +0700

    Add block checkbox-buttons

--- src/pages/UiKit/FormElements/FormElements.js
+++ src/pages/UiKit/FormElements/FormElements.js
@@ -9,14 +9,23 @@ dropdownOpen[1].className += " dropdown__list_open";
 let dropdownToogleNoRadius = document.querySelectorAll('.FormElements .dropdown_rooms .dropdown__toggle');
 dropdownToogleNoRadius[1].className += " dropdown__toogle_noRadius";
 
+// Делаем Expandable Checkbox List как на UiKit по умолчанию
 let expandableCheckboxList = document.querySelectorAll('.expandable-checkbox-list');
 let openCheckboxList = expandableCheckboxList[1].querySelector('.expandable-checkbox-list__title-arrow .arrow');
 let list = expandableCheckboxList[1].querySelector('.expandable-checkbox-list__list');
+    // Стрелачку вверх
 openCheckboxList.classList.add('arrow_up');
+    // Список видимый
 list.classList.remove('expandable-checkbox-list__list_closed');
-
 let checkboxs = expandableCheckboxList[1].querySelectorAll('.checkbox');
-
+    // Отмечаем элементы с 2 по 4
 checkboxs[1].classList.add('checkbox_checked');
 checkboxs[2].classList.add('checkbox_checked');
 checkboxs[3].classList.add('checkbox_checked');
+
+// Делаем Checkbox Buttons как на UiKit по умолчанию
+let checkboxButtons = document.querySelectorAll('.checkbox-buttons');
+let checkbox = checkboxButtons[0].querySelectorAll('.checkbox');
+    // Отмечаем элементы 2 и 3
+checkbox[1].classList.add('checkbox_checked');
+checkbox[2].classList.add('checkbox_checked');
