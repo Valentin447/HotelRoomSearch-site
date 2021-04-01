3d4fe696d150dd59b0449dc4b9500ff048028791
Valentin447 <valentin@myttk.ru>
2021-04-02 00:50:26 +0700

    Add landing page

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ import './scss/utils/fonts.scss';
 import './pages/UiKit/FormElements/FormElements';
 import './pages/UiKit/cards/cards';
 import './blocks/datepicker/datepicker.js';
+import './pages/index/landing-page';
 
