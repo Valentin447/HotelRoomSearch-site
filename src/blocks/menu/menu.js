9c45fdd0b1ab7fbb88252866ee795c3e94bc2651
Valentin447 <valentin@myttk.ru>
2022-02-23 17:33:23 +0700

    Fix blocks: found-rooms, room-card, dropdown

--- src/blocks/menu/menu.js
+++ src/blocks/menu/menu.js
@@ -2,7 +2,6 @@ let menus = document.querySelectorAll('.menu');
 
 for(let menu of menus){
     let burger = menu.querySelector('.menu_burger');
-    console.log('ff');
     burger.onclick = function () {
         
         menu.classList.toggle('menu-list_activ');
